06aeeed06271a84e8e01275800d2057164dc3d1a
Yon2003 <zedrok1@abv.bg>
2026-05-28 11:54:54 +0300

    revert: restore original emojis

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -2,8 +2,7 @@
 import { useState } from 'react';
 import MapModal from './MapModal';
 import { Trip } from '@/types';
-import { formatDuration, formatPrice, getTransportLabel } from '@/lib/utils';
-import TransportImage from '@/components/TransportImage';
+import { formatDuration, formatPrice, getTransportIcon, getTransportLabel } from '@/lib/utils';
 import { Clock, MapPin, Users } from 'lucide-react';
 import Link from 'next/link';
 
@@ -19,7 +18,7 @@ export default function TripCard({ trip }: TripCardProps) {
 
         <div className="flex-1 space-y-3">
           <div className="flex items-center space-x-3">
-            <TransportImage type={trip.type} size={48} />
+            <span className="text-3xl">{getTransportIcon(trip.type)}</span>
             <div>
               <h3 className="font-semibold text-lg text-gray-900">
                 {getTransportLabel(trip.type)}
